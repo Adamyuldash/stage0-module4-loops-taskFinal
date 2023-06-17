73545f390e16b2b33cb9d5bc2f8b1a772b063397
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-06-17 10:35:28 -0700

    12345689

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -4,8 +4,8 @@ public class Power {
     public void printPower(int numberToPrint, int power) {
 
     }
-
     public static void main(String[] args) {
+
         new Power().printPower(10, 3);
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,8 +2,26 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = cathetusLength; j > 0; j--) {
+                if (j <= i + 1) {
+                    System.out.print(j);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 2; j <=cathetusLength ; j++) {
+                if(j<=i+1){
+                    System.out.print(j);
+                }
 
+            }
+
+
+            System.out.println();
+        }
     }
+
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
