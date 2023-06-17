6f1eb507045922f4d0ac6fd0187b80350d60e176
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-06-17 10:45:03 -0700

    1234568999

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,8 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+        int powers =(int)Math.pow(numberToPrint,power);
+        System.out.println(powers);
 
-    }
+        }
+
+
+
     public static void main(String[] args) {
 
         new Power().printPower(10, 3);
